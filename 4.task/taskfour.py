f200b4e8194e1fa36bc7132b054a4004a7960c78
Gölöncsér József <goloncser.j@gmail.com>
2017-09-25 22:50:35 +0200

    refactor taskfour

--- 4.task/taskfour.py
+++ 4.task/taskfour.py
@@ -1,5 +1,5 @@
-'''A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 Ã— 99.
-Find the largest palindrome made from the product of two 3-digit numbers.'''
+'''A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers
+is 9009 = 91 × 99. Find the largest palindrome made from the product of two 3-digit numbers.'''
 
 numberOne = 999
 numberTwo = 999
@@ -10,8 +10,7 @@ for i in xrange(numberOne, 101, -1):
             multiplication = i * j
             multiplicationString = str(multiplication)
             multiplicationStringReverse = multiplicationString[::-1]
-            if(multiplicationString == multiplicationStringReverse):
-                  if(multiplication > biggestPalindromeNumber):
-                        biggestPalindromeNumber = multiplication
+            if((multiplicationString == multiplicationStringReverse) and (multiplication > biggestPalindromeNumber)):
+                  biggestPalindromeNumber = multiplication
 
 print (biggestPalindromeNumber)
